bc5e47e3baf18bcce64fb4e4fe1aafb996f91d37
rhdzmota <rohdzmota@gmail.com>
2018-05-22 16:46:29 -0500

    create simple custom models

--- src/main/scala/com/rhdzmota/chatbot/gateway/model/Content.scala
+++ src/main/scala/com/rhdzmota/chatbot/gateway/model/Content.scala
@@ -3,11 +3,11 @@ package com.rhdzmota.chatbot.gateway.model
 sealed trait Content {
   def mid: String
   def `type`: String
+  def timestamp: Long
 }
 
-case class Text(mid: String,`type`: String, text: String) extends Content
-case class QuickReply(mid: String, `type`: String, payload: String) extends Content
-case class Attachment(mid: String, `type`: String, seq: Int,      url: String) extends Content
-case class Fallback  (mid: String, `type`: String, title: String, url: String) extends Content
-case class Location  (mid: String, `type`: String, lat: Double,   long: Double) extends Content
-
+case class CustomText(mid: String,`type`: String, timestamp: Long, text: String) extends Content
+case class CustomQuickReply(mid: String, `type`: String, timestamp: Long, payload: String) extends Content
+case class CustomAttachment(mid: String, `type`: String, timestamp: Long, seq: Int,      url: String) extends Content
+case class CustomFallback  (mid: String, `type`: String, timestamp: Long, title: String, url: String) extends Content
+case class CustomLocation  (mid: String, `type`: String, timestamp: Long, lat: Double,   long: Double) extends Content
