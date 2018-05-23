147a96d4258160d739076b522c1d129da6b4e9a5
rhdzmota <rohdzmota@gmail.com>
2018-05-23 16:44:39 -0500

    add toMap method

--- src/main/scala/com/rhdzmota/chatbot/gateway/model/CustomMessage.scala
+++ src/main/scala/com/rhdzmota/chatbot/gateway/model/CustomMessage.scala
@@ -1,3 +1,7 @@
 package com.rhdzmota.chatbot.gateway.model
 
-case class CustomMessage(sender: String, receiver: String, timestamp: Long, content: Content)
+case class CustomMessage(sender: String, receiver: String, timestamp: Long, content: Content) {
+  def toMap: Map[String, String] = 
+    Map("sender" -> sender, "receiver" -> receiver) ++ content.toMap
+}
+
