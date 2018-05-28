8be26ac0ea2e488f52396291535e9dbd1911f8de
rhdzmota <rohdzmota@gmail.com>
2018-05-28 15:56:09 -0500

    change data/type

--- src/main/scala/com/rhdzmota/chatbot/gateway/util/EventSyntax.scala
+++ src/main/scala/com/rhdzmota/chatbot/gateway/util/EventSyntax.scala
@@ -19,18 +19,18 @@ object EventSyntax {
           val timestamp   = fbMessage.timestamp
           val messageContentList: List[Option[Content]] = fbMessage.message match {
             case Text(mid, text, quickReply) => quickReply match {
-              case Some(qr) => List(Some(CustomQuickReply(mid, "quickReply", timestamp, qr.payload)))
+              case Some(qr) => List(Some(CustomQuickReply(mid, "rply", timestamp, qr.payload)))
               case None     => List(Some(CustomText(mid, "text", timestamp, text)))
             }
             case WithAttachment(mid, seq, attachments) =>
               attachments.map(attachment => attachment.payload.map(
-                payload => CustomAttachment(mid, attachment.`type`, timestamp, seq, payload.url)))
+                payload => CustomAttachment(mid, attachment.`type`.take(4), timestamp, seq, payload.url)))
             case WithFallback(mid, text, attachments)  =>
               attachments.map(attachment => Some(
-                CustomFallback(mid, attachment.`type`, timestamp, attachment.title, attachment.URL)))
+                CustomFallback(mid, attachment.`type`.take(4), timestamp, attachment.title, attachment.URL)))
             case WithLocation(mid, seq, attachments)   =>
               attachments.map(attachment => attachment.payload.map(
-                payload => CustomLocation(mid, attachment.`type`, timestamp, payload.coordinates.lat, payload.coordinates.long)))
+                payload => CustomLocation(mid, attachment.`type`.take(4), timestamp, payload.coordinates.lat, payload.coordinates.long)))
           }
           messageContentList.map(messageContent => messageContent.map(CustomMessage(senderId, recipientId, entryTime, _)))
         }
